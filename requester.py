b3fc069a850e825a5b5d6c3502de0c00d6058054
Ioan Antonio <ioanantonio1441@gmail.com>
2026-03-23 09:08:42 +0200

    added custom user-agent for bypass bot protection

--- requester.py
+++ requester.py
@@ -3,8 +3,14 @@ import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.5"
+}
+
 def fetch_domain(url):
-    response = requests.get(url, verify=False, timeout=10)
+    response = requests.get(url,headers=HEADERS, verify=False, timeout=10)
 
     response.raise_for_status()
 
