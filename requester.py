e0e230350f40251cbcbeda891b1212506515b288
Ioan Antonio <ioanantonio1441@gmail.com>
2026-03-24 09:04:58 +0200

    added cookie extraction

--- requester.py
+++ requester.py
@@ -15,31 +15,34 @@ def fetch_domain(url):
 
         response.raise_for_status()
 
+        cookies_dict = response.cookies.get_dict()
+
         return {
             "url": url,
             "status_code": response.status_code,
             "html_code": response.text,
-            "headers": dict(response.headers), 
+            "headers": dict(response.headers),
+            "cookies": cookies_dict,
             "error" : None
         }
     except requests.exceptions.Timeout:
-        return {"url": url, "status_code": None, "html_code": "", "headers": {}, "error": "Timeout"}
+        return {"url": url, "status_code": None, "html_code": "", "headers": {}, "cookies": {}, "error": "Timeout"}
         
     except requests.exceptions.ConnectionError:
-        return {"url": url, "status_code": None, "html_code": "", "headers": {}, "error": "ConnectionError"}
+        return {"url": url, "status_code": None, "html_code": "", "headers": {}, "cookies": {}, "error": "ConnectionError"}
         
     except requests.exceptions.HTTPError as e:
-        return {"url": url, "status_code": e.response.status_code, "html_code": "", "headers": {}, "error": f"HTTP {e.response.status_code}"}
+        return {"url": url, "status_code": e.response.status_code, "html_code": "", "headers": {}, "cookies": {}, "error": f"HTTP {e.response.status_code}"}
         
     except requests.exceptions.RequestException as e:
-        return {"url": url, "status_code": None, "html_code": "", "headers": {}, "error": "UnknownRequestError"}
+        return {"url": url, "status_code": None, "html_code": "", "headers": {}, "cookies": {}, "error": "UnknownRequestError"}
 
 if __name__ == "__main__":
     res_1 = fetch_domain("https://example.com")
-    print(f"Error = {res_1['error']}")
+    print(f"URL: {res_1['url']} | Error: {res_1['error']} | Cookies: {res_1['cookies']}")
     
-    res_2 = fetch_domain("https://error6789.ro")
-    print(f"Error = {res_2['error']}")
+    res_2 = fetch_domain("https://www.wikipedia.org/")
+    print(f"URL: {res_2['url']} | Error: {res_2['error']} | Cookies: {res_2['cookies']}")
     
     res_3 = fetch_domain("https://httpstat.us/200?sleep=15000")
-    print(f"Error = {res_3['error']}")
+    print(f"URL: {res_3['url']} | Error: {res_3['error']} | Cookies: {res_3['cookies']}")
